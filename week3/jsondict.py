5b1dec49f32d1e7c3669adc5d4fa2e4ec5f319d4
Grunt Futuk <github@kyber.co.uk>
2017-03-24 16:36:47 +0000

    further simplified script

--- week3/jsondict.py
+++ week3/jsondict.py
@@ -4,29 +4,28 @@ import sys   # so I can read command line arguments
 import json  # so I can read/write data/files in JSON format
 
 # first, add command line arguments to a new dictionary
-args = {}
-counter = 0
+args = {}     # create empty dictionary to add command lines args to
+counter = 0   # initialise counter, so we can reference key value pairs
 for arg in sys.argv:
     args[counter] = arg  # append key value pair using arg position as key
     counter += 1
 
+arglist = []  # new list to hold dictionary of args
+
 # check if file is empty by trying to read it; there are better ways
 f = open('hw3.json', 'r')
-emptyfile = f.readline() == ''
-f.close()
+if f.readline() != '':         # will be True if file is empty
+    f.close()                  # close it so can re-opon at start
+    f = open('hw3.json', 'r')  # open file again, at start
+    arglist = json.load(f)     # read previously saved list of arg dictionaries
+f.close()                      # close file so can reopen for overwrite
 
-# if the file is empty, create list, add dictionary
-if emptyfile:
-    arglist = [args]
-
-# if the file is not empty, retrieve data and add new dictionary
-else:
-    f = open('hw3.json', 'r')
-    arglist = json.load(f)  # read previously saved list of arg dictionaries
-    arglist.append(args)    # add new arguments dictionary to list
-    f.close()               # close file so can reopen for overwrite
+# append new dictionary of command line args to list (empty or reloaded)
+arglist.append(args)           # add new arguments dictionary to list
 
 # write latest version of list of dictionaries to file
 f = open('hw3.json', 'w')
-j = json.dump(arglist, f)  # write (or overwrite previous) dictionary list
+j = json.dump(arglist, f)      # write (or overwrite previous) dictionary list
+
+# all done
 f.close()
