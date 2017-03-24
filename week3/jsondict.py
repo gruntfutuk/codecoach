c7df77f497b31307cf26c9aa8d0050c6e9c034d5
Grunt Futuk <github@kyber.co.uk>
2017-03-24 14:10:01 +0000

    simplified script a little

--- week3/jsondict.py
+++ week3/jsondict.py
@@ -18,20 +18,18 @@ else:
     emptyfile = False
 f.close()  # could reset but this is all we know for now
 
-# if the file is empty, create list, add dictionary, save to file
+# if the file is empty, create list, add dictionary
 if emptyfile:
     arglist = [args]
-    f = open('hw3.json', 'w')
-    j = json.dump(arglist, f)
-    f.close()
 
 # if the file is not empty, retrieve data and add new dictionary
-if not emptyfile:
+else:
     f = open('hw3.json', 'r')
     arglist = json.load(f)  # read previously saved list of arg dictionaries
     arglist.append(args)    # add new arguments dictionary to list
     f.close()               # close file so can reopen for overwrite
 
-    f = open('hw3.json', 'w')
-    j = json.dump(arglist, f)  # overwrite previous list of dictionaries
-    f.close()
+# write latest version of list of dictionaries to file
+f = open('hw3.json', 'w')
+j = json.dump(arglist, f)  # write (or overwrite previous) dictionary list
+f.close()
